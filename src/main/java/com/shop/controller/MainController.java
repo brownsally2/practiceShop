ad05ab7cb94916b4352cbe95177a05b3d106e650
heej0258 <heej0258@naver.com>
2022-11-25 14:30:52 +0900

    commit4

--- src/main/java/com/shop/controller/MainController.java
+++ src/main/java/com/shop/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    // 템플릿의 main으로 이동 하는 것
     @GetMapping("/")
     public String main() {
         return "main";
